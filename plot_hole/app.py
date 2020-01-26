e3977a36adfc67dad4412146f6da241e11083aec
Patrick Trainer <patrick@zlien.com>
2020-01-26 13:54:37 -0600

    fix navbar burger

--- plot_hole/app.py
+++ plot_hole/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, flash, redirect
-from forms import RegistrationForm, LoginForm
+from .forms import RegistrationForm, LoginForm
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -53,7 +53,7 @@ def login():
 
 
 class User(db.Model):
-    id = db.Column(db.Intiger, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20),
@@ -67,13 +67,13 @@ class User(db.Model):
 
 
 class Post(db.Model):
-    id = db.Column(db.Intiger, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120), nullable=False)
     date_posted = db.Column(db.DateTime,
                             nullable=False,
                             default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Intiger, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
         return f"User('{self.title}', '{self.date_posted}')"
