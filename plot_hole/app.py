82fb5711f9f58eac86edb83d87a61963b2ae744b
Patrick Trainer <patrick@zlien.com>
2020-01-26 12:08:45 -0600

    add user and post models

--- plot_hole/app.py
+++ plot_hole/app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, url_for, flash, redirect
-from .forms import RegistrationForm, LoginForm
+from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = 'a4f2d0f8ca8ffdf793fe0b9a0562f32e'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
 
 plots = [{
     '-90.7314': '89.6431',
@@ -47,3 +50,30 @@ def login():
             # flash('You have been logged in!', 'success')
             return redirect(url_for('home'))
     return render_template('login.html', title='Login', form=form)
+
+
+class User(db.Model):
+    id = db.Column(db.Intiger, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20),
+                           nullable=False,
+                           default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Intiger, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    date_posted = db.Column(db.DateTime,
+                            nullable=False,
+                            default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Intiger, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.title}', '{self.date_posted}')"
