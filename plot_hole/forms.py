8b3504101d3ea16e00fb5429dc14882349e8022e
Patrick Trainer <patrick@zlien.com>
2020-01-26 16:33:56 -0600

    formatting

--- plot_hole/forms.py
+++ plot_hole/forms.py
@@ -4,23 +4,16 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
 class RegistrationForm(FlaskForm):
-
     email = StringField('email', validators=[DataRequired(), Email()])
-
     password = PasswordField('password', validators=[DataRequired()])
-
     confirm_password = PasswordField(
         validators=[DataRequired(),
                     EqualTo('password', 'use same password!')])
-
     submit = SubmitField()
 
 
 class LoginForm(FlaskForm):
     email = StringField('email', validators=[DataRequired(), Email()])
-
     password = PasswordField('password', validators=[DataRequired()])
-
     # remember = BooleanField('remember')
-
     submit = SubmitField()
