5f3c03f9990d22fcdb66cc1c1c148715fd79caab
Patrick Trainer <Patrick@Corals-MacBook-Air.local>
2020-02-25 09:47:26 -0600

    add about page

--- plot_hole/main/routes.py
+++ plot_hole/main/routes.py
@@ -6,3 +6,8 @@ main = Blueprint("main", __name__)
 @main.route("/", methods=("GET", "POST"))
 def home():
     return render_template("home.html")
+
+
+@main.route("/about")
+def about():
+    return render_template("about.html")
